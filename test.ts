e432d4c95fb43f8ef39914f20bd3447afd87dafb
Albert Diones <adiones@addteam.org>
2024-07-30 21:24:21 +0800

    use cache-via-redis instead of cache-via-mongo

--- test.ts
+++ test.ts
@@ -1,5 +1,19 @@
+import cacheViaRedis from "cache-via-redis";
 import loadSecrets from ".";
+import profile from 'timer-profiler';
+
 
 export const secretKey = 'CarrotsRabbit===================';
 
-console.log(await loadSecrets('test-secrets.json', {key: secretKey}));
+cacheViaRedis.connect();
+
+console.time();
+profile(
+    () => loadSecrets('test-secrets.json', {key: secretKey})
+)
+.then(
+    (results) => {
+        console.timeEnd();
+        return console.log('results', results)
+    }
+);
